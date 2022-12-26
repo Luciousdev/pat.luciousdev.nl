e21c3f4ea826c7884c69618bcd54c559ffb2a357
Lucy <lucy.puyenbroek@gmail.com>
2022-12-26 23:43:00 +0100

    changed to switch statement for achievements

--- assets/backend/achievements.js
+++ assets/backend/achievements.js
@@ -10,21 +10,27 @@ let top1Unlocked = false;
 function achievements() {
     score = sessionStorage.getItem("session");
 
-    if (score == 100 && !newbieUnlocked) {
-        alert("Achievement unlocked!\n\nNewbie");
-        newbieUnlocked = true;
-    } else if (score == 500 && !gettingThereUnlocked) {
-        alert("Achievement unlocked!\n\nYou're getting there!");
-        gettingThereUnlocked = true;
-    } else if (score == 10000 && !climbingUnlocked) {
-        alert("Achievement unlocked!\n\nYou're climbing the leaderboards!");
-        climbingUnlocked = true;
-    } else if (score == 69420 && !niceUnlocked) {
-        alert("Achievement unlocked!\n\nNice!");
-        niceUnlocked = true;
-    } else if (score == 1000000 && !top1Unlocked) {
-        alert("Achievement unlocked!\n\nYou're in the top 1% (I think)!");
-        top1Unlocked = true;
+    switch (true) {
+        case score == 100 && !newbieUnlocked:
+            alert("Achievement unlocked!\n\nNewbie");
+            newbieUnlocked = true;
+            break;
+        case score == 500 && !gettingThereUnlocked:
+            alert("Achievement unlocked!\n\nYou're getting there!");
+            gettingThereUnlocked = true;
+            break;
+        case score == 10000 && !climbingUnlocked:
+            alert("Achievement unlocked!\n\nYou're climbing the leaderboards!");
+            climbingUnlocked = true;
+            break;
+        case score == 69420 && !niceUnlocked:
+            alert("Achievement unlocked!\n\nNice!");
+            niceUnlocked = true;
+            break;
+        case score == 1000000 && !top1Unlocked:
+            alert("Achievement unlocked!\n\nYou're in the top 1% (I think)!");
+            top1Unlocked = true;
+            break;
     }
 }
 
--- assets/backend/localclickcount.js
+++ assets/backend/localclickcount.js
@@ -1,36 +1,20 @@
-let totalClicked = localStorage.getItem("localpats");
-if (totalClicked === null) {
-    totalClicked = 0;
-}
-let click = false;
+// FIXME
+// let totalClicked = 0;
 
-function session() {
-    localStorage.getItem("localpats");
+// // If the "totalPats" key exists in localStorage, retrieve its value and assign it to totalClicked
+// if (localStorage.getItem("totalPats")) {
+//     totalClicked = localStorage.getItem("totalPats");
+// }
 
-    let clickImage = document.getElementById("clicker");
-    clickImage.addEventListener("click", debounce(function() {
-        click = true;
-        totalClicked += 1;
-        localStorage.setItem("localpats", totalClicked);
-    }, 200));
+// let imageClicks = document.getElementById("clicker");
+// imageClicks.addEventListener("click", function() {
+//     totalClicked += 1;
+//     localStorage.setItem("totalPats", totalClicked);
+// });
 
-    if (click) {
-        localStorage.setItem("localpats", totalClicked);
-    }
-}
-session();
-
-function setLocal() {
-    let totalLocal = localStorage.getItem("localpats");
-    document.getElementById("totalPats").innerHTML = totalLocal;
-}
-setInterval(setLocal, 250);
-setLocal();
-
-function debounce(func, wait) {
-    let timeout;
-    return function() {
-        clearTimeout(timeout);
-        timeout = setTimeout(func, wait);
-    };
-}
+// function setLocal() {
+//     let totalLocal = localStorage.getItem("totalPats");
+//     document.getElementById("totalPats").innerHTML = totalLocal;
+// }
+// setInterval(setLocal, 250);
+// setLocal();
