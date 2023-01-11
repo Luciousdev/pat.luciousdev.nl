a2b5c64252a15128f95b3c1dc0d49267df6a0c40
Luciousdev <lucy.puyenbroek@gmail.com>
2023-01-11 11:05:25 +0100

    fixed so if you already had a score the achievements will still add.

--- assets/backend/achievements.js
+++ assets/backend/achievements.js
@@ -13,37 +13,37 @@ function achievements() {
     score = localStorage.getItem("localPats");
 
     switch (true) {
-        case score == 100 && !newbieUnlocked:
+        case score >= 100 && !newbieUnlocked:
             alert("Achievement unlocked!\n\nNewbie");
             newbieUnlocked = true;
             document.getElementById("copper").style.visibility = "visible";
             break;
-        case score == 500 && !gettingThereUnlocked:
+        case score >= 500 && !gettingThereUnlocked:
             alert("Achievement unlocked!\n\nYou're getting there!");
             gettingThereUnlocked = true;
             document.getElementById("bronze").style.visibility = "visible";
             break;
-        case score == 2000 && !climbingUnlocked:
+        case score >= 2000 && !climbingUnlocked:
             alert("Achievement unlocked!\n\nYou're climbing the leaderboards!");
             climbingUnlocked = true;
             document.getElementById("silver").style.visibility = "visible";
             break;
-        case score == 5000 && !gold:
+        case score >= 5000 && !gold:
             alert("Achievement unlocked!\n\nGood job!");
             gold = true;
             document.getElementById("gold").style.visibility = "visible";
             break;
-        case score == 15000 && !platinum:
+        case score >= 15000 && !platinum:
             alert("Achievement unlocked!\n\nGood job!");
             platinum = true;
             document.getElementById("platinum").style.visibility = "visible";
             break;
-        case score == 69420 && !niceUnlocked:
+        case score >= 69420 && !niceUnlocked:
             alert("Achievement unlocked!\n\nNice!");
             document.getElementById("emerald").style.visibility = "visible";
             niceUnlocked = true;
             break;
-        case score == 1000000 && !top1Unlocked:
+        case score >= 1000000 && !top1Unlocked:
             alert("Achievement unlocked!\n\nYou're in the top 1% (I think)!");
             document.getElementById("diamond").style.visibility = "visible";
             top1Unlocked = true;
